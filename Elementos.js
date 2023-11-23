c0455268fe95a2035b29ed7217d283fec01e594e
Batallon501 <pcastillo1801otmail.com>
2023-11-23 09:12:20 -0600

    first commit

---
+++ Elementos.js
@@ -0,0 +1,29 @@
+
+/*JUAN CORTES*/
+
+hamburger = document.querySelector(".hamburger");
+hamburger.onclick= function() {
+navbar = document.querySelector(".nav-bar");
+navbar.classList.toggle("active");
+}
+/*JUAN CORTES FIN*/
+
+/*PABLO*/
+/*PABLO FIN*/
+
+/*JUAN DE LA CRUZ*/
+document.getElementById('next').onclick = function(){
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide').appendChild(lists[0]);
+}
+document.getElementById('prev').onclick = function(){
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide').prepend(lists[lists.length - 1]);
+}
+/*FIN JUAN DE LA CRUZ*/
+
+/*ALAN INICIO*/
+
+
+
+/*ALAN FIN*/
