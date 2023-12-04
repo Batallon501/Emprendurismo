5591cae0bae023f1883b42572470d6aa1c436762
Batallon501 <147020917+Batallon501@users.noreply.github.com>
2023-12-04 08:34:38 -0600

    Update Elementos.js

--- Elementos.js
+++ Elementos.js
@@ -88,14 +88,6 @@ formulario.addEventListener('submit', (e) => {
 /*PABLO FIN*/
 
 /*JUAN DE LA CRUZ*/
-document.getElementById('next').onclick = function(){
-    let lists = document.querySelectorAll('.item');
-    document.getElementById('slide').appendChild(lists[0]);
-}
-document.getElementById('prev').onclick = function(){
-    let lists = document.querySelectorAll('.item');
-    document.getElementById('slide').prepend(lists[lists.length - 1]);
-}
 /*FIN JUAN DE LA CRUZ*/
 
 /*ALAN INICIO*/
